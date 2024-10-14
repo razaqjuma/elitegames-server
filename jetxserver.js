e7183ae7b39f0dd48e43433653dae43c54a36a55
ABDULRAZAQ <razaqjuma@pop-os.localdomain>
2024-10-14 19:42:56 +0300

    Third Commit

--- jetxserver.js
+++ jetxserver.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mysql = require('mysql');
+const mysql = require('mysql2');
+
 
 const app = express();
 const port = 3000;
